64ce6bfbfadfb86ca29d8483d1c07c63cf107edd
cherfaoui-syphax <syphax2000.cherfaoui@gmail.com>
2025-11-12 02:17:03 +0100

    remove loader

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    loader: { ".js": "jsx" }, // Treat .js files as JSX
 
     // allow .js (and .jsx/.ts/.tsx if you like) to be parsed as JSX
     include: /src\/.*\.(js|jsx|ts|tsx)$/,
